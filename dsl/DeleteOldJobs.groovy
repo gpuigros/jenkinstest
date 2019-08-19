9e6ff5f4a923ed5538a294f1a262df8aa028b586
Guillem Puigròs Gómez <gpuigros@hotelbeds.com>
2019-08-19 18:40:39 +0200

    d

--- dsl/DeleteOldJobs.groovy
+++ dsl/DeleteOldJobs.groovy
@@ -6,7 +6,7 @@ class DeleteOldJobs  {
     def deleteOld(String folderName) {
         println "in"
         def counter = 0
-        jobs = Jenkins.instance.getAllItems()
+        def jobs = Jenkins.instance.getAllItems()
         for (job in jobs) {
                 println "Job= '${counter++}' '${job.name}' scm '${job.scm}'"
 
