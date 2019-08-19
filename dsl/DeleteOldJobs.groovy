5f807f4b969bcb1c6a8164bfae5ace64abd7cbf8
Guillem Puigròs Gómez <gpuigros@hotelbeds.com>
2019-08-19 18:39:33 +0200

    D

--- dsl/DeleteOldJobs.groovy
+++ dsl/DeleteOldJobs.groovy
@@ -4,8 +4,13 @@ import hudson.model.*
 class DeleteOldJobs  {
 
     def deleteOld(String folderName) {
+        println "in"
+        counter = 0
+        jobs = Jenkins.instance.getAllItems()
+        for (job in jobs) {
+                println "Job= '${counter++}' '${job.name}' scm '${job.scm}'"
 
-        println Jenkins.instance.projects.collect { it.name }
+        }
 }
 }
 
