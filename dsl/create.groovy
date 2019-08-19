4f44c432d9d27fdfab847c9717f70493a1f7b61a
Guillem Puigròs Gómez <gpuigros@hotelbeds.com>
2019-08-19 16:42:37 +0200

    d

--- dsl/create.groovy
+++ dsl/create.groovy
@@ -1,10 +1,14 @@
-job('DSL/job-dsl-test') {
+job('DSL/job-dsl-test_BUILD') {
     scm {
         git('git://github.com/gpuigros/jenkinstest.git')
         //credentials('gpuigros-github')
+        extensions {
+                wipeOutWorkspace()
+                localBranch master
+            }
     }
     
     steps {
-        maven('-e clean pacakage')
+        maven('-e clean package')
     }
 }
