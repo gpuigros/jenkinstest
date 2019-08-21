991a529ed2eb2c7c8fbae6d8977be5117351a9cd
Guillem Puigròs Gómez <gpuigros@hotelbeds.com>
2019-08-21 14:18:44 +0200

    s

--- dsl/DeleteOldJobs.groovy
+++ dsl/DeleteOldJobs.groovy
@@ -13,8 +13,8 @@ class DeleteOldJobs  {
     }
     def deleteOld(String folderName,String includeRegexp,String excludeRegexp) {
         out.println "Cleaning folder ${folderName}"
-        out.println "includeRegexp ${includeRegexp}"
-        out.println "excludeRegexp ${excludeRegexp}"
+        out.println "includeRegexp ${include}"
+        out.println "excludeRegexp ${exclude}"
         /**AbstractFolder folder = Jenkins.instance.getAllItems(AbstractFolder.class)
             .find {folder -> folderName == folder.fullName };
         folder.getAllJobs()
@@ -22,14 +22,14 @@ class DeleteOldJobs  {
             .findAll {job -> job.isBuildable()}
             .each {out.println "Job= in ${folderName} ${it.name}"};
         **/
-        def includePattern = Pattern.compile(includeRegexp)
-        def excludePattern = Pattern.compile(excludeRegexp)
+        def includePattern = Pattern.compile(include)
+        def excludePattern = Pattern.compile(exclude)
 
         def jobs = Jenkins.instance.getAllItems()
         for (job in jobs) {
                 if (!excludePattern.matcher(job.name).matches()){
                     def matcher=includePattern.matcher(job.name)
-                    if (matcher!=null && matcher.group()!=null){
+                    if (job.name.contains(includeRegexp)){
                         out.println "Job= ${job} ${counter++} ${job.name} ${job.description}  ${job.displayName}"
                     }
                 }
